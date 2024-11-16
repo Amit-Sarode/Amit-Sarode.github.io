4bdd6919519cd9f94b18d48bb23ea49fe5d9ed0a
Amit <sarodeamit990@gmail.com>
2024-11-16 19:14:36 +0530

    first commit

--- script.js
+++ script.js
@@ -10,13 +10,27 @@ document.getElementById("theme-switch").addEventListener("change", () => {
     document.body.classList.toggle("dark-theme");
 });
 
-// const projectCards = document.querySelectorAll(".projectsCard");
-// projectCards.forEach((card) => {
-//     const info = card.querySelector(".info");
-//     card.addEventListener("mouseenter", () => {
-//         gsap.to(info, { opacity: 1, transform: "translateY(0)", duration: 0.5 });
-//     });
-//     card.addEventListener("mouseleave", () => {
-//         gsap.to(info, { opacity: 0, transform: "translateY(20px)", duration: 0.5 });
-//     });
-// });
+gsap.from("#projectsCard", {
+    y: 100,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#projectsCard",
+        scroller: "body",
+        // markers: true,
+        start: "top 80%",
+        end: "top 50%",
+        scrub: true,
+    },
+});
+gsap.from("#skills", {
+    y: 100,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#skills",
+        scroller: "body",
+        // markers: true,
+        start: "top 80%",
+        end: "top 50%",
+        scrub: true,
+    },
+});
