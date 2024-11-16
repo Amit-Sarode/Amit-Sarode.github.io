101e8999674233e8459f3dc3ea0f87d976276ae4
Amit <sarodeamit990@gmail.com>
2024-11-16 12:28:47 +0530

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const container = document.querySelector("#container")
-gsap.from(container, {
-    duration: 2,
-    y: -60,
-    ease: "linear",
-})
-const theme = document.querySelector("#theme-switch");
-theme.addEventListener("click", () => {
+
+gsap.from("#container", {
+    duration: 3,
+    y: -100,
+    opacity: 0,
+    ease: "power3.out",
+});
+
+document.getElementById("theme-switch").addEventListener("change", () => {
     document.body.classList.toggle("dark-theme");
 });
+
+// const projectCards = document.querySelectorAll(".projectsCard");
+// projectCards.forEach((card) => {
+//     const info = card.querySelector(".info");
+//     card.addEventListener("mouseenter", () => {
+//         gsap.to(info, { opacity: 1, transform: "translateY(0)", duration: 0.5 });
+//     });
+//     card.addEventListener("mouseleave", () => {
+//         gsap.to(info, { opacity: 0, transform: "translateY(20px)", duration: 0.5 });
+//     });
+// });
