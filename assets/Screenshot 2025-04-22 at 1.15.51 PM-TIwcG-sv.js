6b2d594c270f008781f42b9598b939852a5adc5c
Amit Sarode <sarodeamit990@gmail.com>
2026-05-12 16:20:31 +0530

    Updates

---
+++ assets/Screenshot 2025-04-22 at 1.15.51 PM-TIwcG-sv.js
@@ -0,0 +1,1 @@
+const s="/assets/doctor-preview-DxW4Wr9K.png",o="/assets/Screenshot%202025-04-22%20at%201.15.51%E2%80%AFPM-Bu2Gh-oH.png";export{s as d,o as e};
---
+++ assets/profileImg-BvsdbHUe.js
@@ -0,0 +1,1 @@
+const o="/assets/profileImg-O0XEoNqO.png";export{o as p};
